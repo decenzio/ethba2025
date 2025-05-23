27f8dbe2f2757edd311a99e02e4c8f17b77ce4e6
roman <xromanb@proton.me>
2025-05-24 01:40:38 +0200

    adapt to base

--- packages/nextjs/services/connectService.ts
+++ packages/nextjs/services/connectService.ts
@@ -4,7 +4,7 @@ import { useGlobalState } from "~~/services/store/store";
 import type { ConnectService, WalletInfo } from "~~/types/import";
 
 import { createSmartAccountClient } from "permissionless";
-import { sepolia } from "viem/chains";
+import { base } from "viem/chains";
 import { http, createPublicClient } from "viem";
 import { toNostrSmartAccount } from "./accountService";
 import { getPublicClient } from "wagmi/actions";
@@ -29,7 +29,7 @@ export const connectService = {
     useGlobalState.getState().setWalletInfo(walletInfo);
     useGlobalState.getState().setNPubKey(nPubkey);
 
-    const publicClient = createPublicClient({chain: sepolia, transport: http("https://eth-sepolia.public.blastapi.io")});
+    const publicClient = createPublicClient({chain: base, transport: http("https://base-mainnet.public.blastapi.io")});
 
     const account = await toNostrSmartAccount({
       client: publicClient,
@@ -39,9 +39,9 @@ export const connectService = {
     // Create the required clients.
     const bundlerClient = createSmartAccountClient({
       account,
-      chain: sepolia,
+      chain: base,
       bundlerTransport: http(
-        `https://api.pimlico.io/v2/11155111/rpc?apikey=pim_X5CHVGtEhbJLu7Wj4H8fDC`,
+        `https://api.pimlico.io/v2/8453/rpc?apikey=pim_X5CHVGtEhbJLu7Wj4H8fDC`,
       ), // Use any bundler url
     });
 
@@ -51,10 +51,10 @@ export const connectService = {
 
     const txHash = await bundlerClient.sendTransaction({
       to: '0x66bAd48301609adaa01CB3140D1b1D92bFa03dD5', // address you want to send to
-      value: BigInt(1e14),      // amount in wei (e.g., 0.01 ETH)
+      value: BigInt(1e13),      // amount in wei (e.g., 0.01 ETH)
       data: '0x',               // optional calldata, '0x' for simple ETH transfer
-      maxFeePerGas: estimateFees.maxFeePerGas,
-      maxPriorityFeePerGas: estimateFees.maxPriorityFeePerGas,
+      maxFeePerGas: estimateFees.maxFeePerGas * 15n,
+      maxPriorityFeePerGas: 1250000n,
     });
 
   console.log('UserOperation hash:', txHash);
