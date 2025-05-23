1c862eac2d301a144c2d5cac0e6c5c48d8974276
roman <xromanb@proton.me>
2025-05-23 19:30:05 +0200

    Update WalletInformation.tsx

--- packages/nextjs/components/Wallet/WalletInformation.tsx
+++ packages/nextjs/components/Wallet/WalletInformation.tsx
@@ -1,3 +1,4 @@
+import { Balance } from "../scaffold-eth/Balance";
 import WalletInteraction from "~~/components/Wallet/WalletInteraction";
 import { useGlobalState } from "~~/services/store/store";
 
@@ -13,7 +14,7 @@ const WalletInformation = ({ className }: { className?: string }) => {
       <div className="card bg-gradient-to-r from-accent-content via-secondary/70 to-accent shadow-xl text-white w-110">
         <div className="card-body">
           <h2 className="card-title text-lg font-semibold">Wallet Balance</h2>
-          <p className="text-5xl font-bold">6.23 ETH</p>
+          <Balance address={"vitalik.eth"} />
           <div className="card-actions justify-end mt-4">
             <WalletInteraction />
           </div>
