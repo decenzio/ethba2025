7ee7fefdf84c41b9600637a8ef0db9c8d600a530
tibordulovec <tibordulovec@riesenia.com>
2025-05-24 14:07:40 +0200

    Added option to search for nbpum without connecting

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -40,16 +40,19 @@ const Home: NextPage = () => {
         {pubkey ? (
           <div className={"flex flex-col gap-8 items-center justify-center pb-10 grow h-fill"}>
             <WalletCard className="opacity-0 animate-slideIn" />
-            <button className="btn btn-dash btn-success" onClick={() => handleOpenModal("wallet-search-modal")}>
+            <button
+              className="btn btn-dash btn-success animate-slideIn"
+              onClick={() => handleOpenModal("wallet-search-modal")}
+            >
               <MagnifyingGlassCircleIcon className="h-6 w-6 mr-2 inline" />
-              Search address of recipient
+              Search address of npub
             </button>
           </div>
         ) : (
           <div className="px-20 flex flex-col gap-4 m-auto w-fit justify-center items-start grow h-fill text-4xl">
             <div className="flex flex-col gap-4 animate-fadeIn item-start">
               <p className="text-5xl m-0 opacity-0 animate-slideIn delay-100">Hello👋</p>
-              <p className="m-0 opacity-0 animate-slideIn delay-500">
+              <p className="m-0 opacity-0 animate-slideIn delay-500 ">
                 to continue, <br />
                 please{" "}
                 <u className="hover:no-underline cursor-pointer" onClick={handleConnectButton}>
@@ -64,6 +67,19 @@ const Home: NextPage = () => {
                 >
                   Connect <ArrowRightIcon className="h-6 w-6 ml-2 inline" />
                 </button>
+              </div>
+              <div className="opacity-0 animate-slideIn delay-1500">
+                <hr className="opacity-20 mt-5" />
+                <p className="opacity-70 text-base hover:opacity-100 transition-all">
+                  <div className="mb-3">Or just search for a wallet address using the npub </div>
+                  <button
+                    className="btn btn-dash hover:btn-outline hover:border-white"
+                    onClick={() => handleOpenModal("wallet-search-modal")}
+                  >
+                    <MagnifyingGlassCircleIcon className="h-6 w-6 mr-2 inline" />
+                    Search address of npub
+                  </button>
+                </p>
               </div>
             </div>
           </div>
